4c57a960f5d4a942c6e72ee9fd76d98d2cbf79bf
HHK <hhk.contact@protonmail.com>
2021-11-10 19:36:00 +0100

    style: improve network visibility

--- src/helpers/network.ts
+++ src/helpers/network.ts
@@ -16,62 +16,75 @@ export interface INetwork {
     rpc: string
 }
 
+export enum CHAIN_IDS {
+    ETHEREUM = 1,
+    BSC = 56,
+    POLYGON = 137,
+    XDAI = 100,
+    FANTOM = 250,
+    MOONRIVER = 1285,
+    ARBITRUM = 42161,
+    CELO = 42220,
+    AVALANCHE = 43114,
+    HARMONY = 1666600000
+}
+
 export const NETWORKS: {[id: number]: INetwork} = {
-    42161: {
+    [CHAIN_IDS.ARBITRUM]: {
         id: 42161,
         name: "Arbitrum",
         logo: arbitrum,
         rpc: "https://arb1.arbitrum.io/rpc"
     },
-    43114: {
+    [CHAIN_IDS.AVALANCHE]: {
         id: 43114,
         name: "Avalanche",
         logo: avalanche,
         rpc: "https://api.avax.network/ext/bc/C/rpc"
     },
-    56: {
+    [CHAIN_IDS.BSC]: {
         id: 56,
         name: "BSC",
         logo: bsc,
         rpc: "https://bsc-dataseed1.binance.org"
     },
-    42220: {
+    [CHAIN_IDS.CELO]: {
         id: 42220,
         name: "Celo",
         logo: celo,
         rpc: "https://forno.celo.org"
     },
-    1: {
+    [CHAIN_IDS.ETHEREUM]: {
         id: 1,
         name: "Ethereum",
         logo: ethereum,
         rpc: "https://api.mycryptoapi.com/eth"
     },
-    250: {
+    [CHAIN_IDS.FANTOM]: {
         id: 250,
         name: "Fantom",
         logo: fantom,
         rpc: "https://rpc.ftm.tools"
     },
-    1666600000: {
+    [CHAIN_IDS.HARMONY]: {
         id: 1666600000,
         name: "Harmony",
         logo: harmony,
         rpc: "https://api.harmony.one"
     },
-    1285: {
+    [CHAIN_IDS.MOONRIVER]: {
         id: 1285,
         name: "Moonriver",
         logo: moonriver,
         rpc: "https://rpc.moonriver.moonbeam.network"
     },
-    137: {
+    [CHAIN_IDS.POLYGON]: {
         id: 137,
         name: "Polygon",
         logo: polygon,
         rpc: "https://polygon-rpc.com/"
     },
-    100: {
+    [CHAIN_IDS.XDAI]: {
         id: 100,
         name: "XDAI",
         logo: xdai,
--- src/helpers/products.ts
+++ src/helpers/products.ts
@@ -3,11 +3,7 @@ import bentoBoxLogo from "./../imports/images/products/bentoBox.png";
 import kashiABI from "./../imports/abis/kashi.json";
 import bentoBoxABI from "./../imports/abis/bento.json";
 import boringHelperABI from "./../imports/abis/boringhelper.json";
-import kashiPairsEthereum from "./../imports/kashiPairs/ethereum.json";
-import kashiPairsArbitrum from "./../imports/kashiPairs/arbitrum.json";
-import kashiPairsBsc from "./../imports/kashiPairs/bsc.json";
-import kashiPairsPolygon from "./../imports/kashiPairs/polygon.json";
-import kashiPairsXdai from "./../imports/kashiPairs/xdai.json";
+import { CHAIN_IDS } from "./network";
 
 export interface IProduct {
     name: string,
@@ -19,31 +15,6 @@ export interface IProduct {
     ABI: any
 }
 
-export interface IToken {
-    address: string,
-    name: string,
-    symbol: string,
-    decimals: number,
-    logoURI: string
-}
-
-export interface IKashiPairData {
-    address: string,
-    asset: IToken,
-    collateral: IToken,
-    lendAPR: number,
-    borrowAPR: number
-    utilization: number
-}
-
-export const KASHI_PAIRS: {[id: number]: string[]} = {
-    1: kashiPairsEthereum,
-    42161: kashiPairsArbitrum,
-    56: kashiPairsBsc,
-    137: kashiPairsPolygon,
-    100: kashiPairsXdai
-}
-
 export const PRODUCTS: {[name: string]: IProduct} = {
     "Kashi": {
         name: "Kashi",
@@ -52,11 +23,11 @@ export const PRODUCTS: {[name: string]: IProduct} = {
         description: "Isolated lending and borrowing.",
         active: true,
         networks: {
-            1: "0x2cba6ab6574646badc84f0544d05059e57a5dc42",
-            42161: "0xa010ee0226cd071bebd8919a1f675cae1f1f5d3e",
-            56: "0x2cba6ab6574646badc84f0544d05059e57a5dc42",
-            137: "0xb527c5295c4bc348cbb3a2e96b2494fd292075a7",
-            100: "0x7a6da9903d0a481f40b8336c1463487bc8c0407e"
+            [CHAIN_IDS.ETHEREUM]: "0x2cba6ab6574646badc84f0544d05059e57a5dc42",
+            [CHAIN_IDS.ARBITRUM]: "0xa010ee0226cd071bebd8919a1f675cae1f1f5d3e",
+            [CHAIN_IDS.BSC]: "0x2cba6ab6574646badc84f0544d05059e57a5dc42",
+            [CHAIN_IDS.POLYGON]: "0xb527c5295c4bc348cbb3a2e96b2494fd292075a7",
+            [CHAIN_IDS.XDAI]: "0x7a6da9903d0a481f40b8336c1463487bc8c0407e"
         },
         ABI: kashiABI
     },
@@ -67,11 +38,11 @@ export const PRODUCTS: {[name: string]: IProduct} = {
         description: "Interest bearing vault.",
         active: true,
         networks: {
-            1: "0xf5bce5077908a1b7370b9ae04adc565ebd643966",
-            42161: "0x74c764D41B77DBbb4fe771daB1939B00b146894A",
-            56: "0xf5bce5077908a1b7370b9ae04adc565ebd643966",
-            137: "0x0319000133d3ada02600f0875d2cf03d442c3367",
-            100: "0xE2d7F5dd869Fc7c126D21b13a9080e75a4bDb324"
+            [CHAIN_IDS.ETHEREUM]: "0xf5bce5077908a1b7370b9ae04adc565ebd643966",
+            [CHAIN_IDS.ARBITRUM]: "0x74c764D41B77DBbb4fe771daB1939B00b146894A",
+            [CHAIN_IDS.BSC]: "0xf5bce5077908a1b7370b9ae04adc565ebd643966",
+            [CHAIN_IDS.POLYGON]: "0x0319000133d3ada02600f0875d2cf03d442c3367",
+            [CHAIN_IDS.XDAI]: "0xE2d7F5dd869Fc7c126D21b13a9080e75a4bDb324"
         },
         ABI: bentoBoxABI
     },
@@ -82,11 +53,11 @@ export const PRODUCTS: {[name: string]: IProduct} = {
         description: "",
         active: false,
         networks: {
-            1: "0x11Ca5375AdAfd6205E41131A4409f182677996E6",
-            42161: "0x37B3287292De241278fB5FCa514a756E0BE924f8",
-            56: "0x11Ca5375AdAfd6205E41131A4409f182677996E6",
-            137: "0xA77a7fD5a16237B85E0FAd02C51f459D18AE93Cd",
-            100: "0x97e4a0fb71243A83A6FbaEF7Cf73617594e4cF2F"
+            [CHAIN_IDS.ETHEREUM]: "0x11Ca5375AdAfd6205E41131A4409f182677996E6",
+            [CHAIN_IDS.ARBITRUM]: "0x37B3287292De241278fB5FCa514a756E0BE924f8",
+            [CHAIN_IDS.BSC]: "0x11Ca5375AdAfd6205E41131A4409f182677996E6",
+            [CHAIN_IDS.POLYGON]: "0xA77a7fD5a16237B85E0FAd02C51f459D18AE93Cd",
+            [CHAIN_IDS.XDAI]: "0x97e4a0fb71243A83A6FbaEF7Cf73617594e4cF2F"
         },
         ABI: boringHelperABI
     }
