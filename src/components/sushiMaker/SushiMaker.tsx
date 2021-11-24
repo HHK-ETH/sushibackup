a81c8b50b1ad7beb6af02255b055e83c796e6ec9
HHK <hhk.contact@protonmail.com>
2021-11-24 19:13:18 +0100

    feat: fetchPairs and display values sushimaker

--- src/components/sushiMaker/SushiMaker.tsx
+++ src/components/sushiMaker/SushiMaker.tsx
@@ -1,16 +1,39 @@
-import { useState } from "react";
+import { Web3Provider } from "@ethersproject/providers";
+import { useWeb3React } from "@web3-react/core";
+import { providers } from "ethers";
+import { useEffect, useState } from "react";
+import { getAllpairs, IPairData } from "./../../helpers/sushiMaker";
 
 export function SushiMaker(): JSX.Element {
-  const [loading, setLoading] = useState(false);
-  const [pairs, setPairs] = useState([]);
+  const context = useWeb3React<Web3Provider>();
+  const { account, active, chainId, connector } = context;
+  const [loading, setLoading]: [boolean, Function] = useState(false);
+  const [pairs, setPairs]: [IPairData[], Function] = useState([]);
+
+  useEffect(() => {
+    async function fetchPairs() {
+      if (!connector || !chainId) return;
+      setLoading(true);
+      const web3Provider = new providers.Web3Provider(
+        await connector.getProvider(),
+        "any"
+      );
+      setPairs(((await getAllpairs(web3Provider, chainId)).sort((pairA, pairB) => {
+        if (pairA.value > pairB.value) return -1;
+        return +1;
+      })));
+      setLoading(false);
+    }
+    fetchPairs();
+  }, [active, chainId, connector]);
 
   return (
     <div className="container p-16 mx-auto text-center text-white">
       {loading && <div className={"text-white"}>loading data...</div>}
       <div className="grid grid-cols-6 py-8 bg-indigo-900 rounded-t-xl">
         <div className="">Pair</div>
-        <div className="">Token A</div>
-        <div className="">Token B</div>
+        <div className="col-span-2">Token A</div>
+        <div className="col-span-2">Token B</div>
         <div className="">Value</div>
       </div>
       {pairs.map((pair, i) => {
@@ -19,10 +42,10 @@ export function SushiMaker(): JSX.Element {
             key={i}
             className="grid grid-cols-6 py-4 bg-indigo-900 cursor-pointer bg-opacity-60 hover:bg-opacity-75"
           >
-            <div className="">pair</div>
-            <div className="">token a</div>
-            <div className="">token b</div>
-            <div className="">value</div>
+            <div className="">{pair.name}</div>
+            <div className="col-span-2">{pair.tokenA}</div>
+            <div className="col-span-2">{pair.tokenB}</div>
+            <div className="">{pair.value.toFixed(2)}$</div>
           </div>
         );
       })}
---
+++ src/helpers/sushiMaker.ts
@@ -0,0 +1,117 @@
+import { Web3Provider } from "@ethersproject/providers";
+import { BigNumber, Contract } from "ethers";
+import { PRODUCTS } from "./products";
+import { getToken } from "./../imports/tokens";
+import { formatUnits } from "ethers/lib/utils";
+import { NETWORKS } from "./network";
+
+export interface IPairData {
+  name: string;
+  tokenA: string;
+  tokenB: string;
+  value: number;
+}
+
+export async function getAllpairs(
+  web3Provider: Web3Provider,
+  chainId: number
+): Promise<IPairData[]> {
+  const sushiFactory: Contract = new Contract(
+    PRODUCTS["SushiMaker"].networks[chainId],
+    PRODUCTS["SushiMaker"].ABI,
+    web3Provider
+  );
+  const boringHelper: Contract = new Contract(
+    PRODUCTS["BoringHelper"].networks[chainId],
+    PRODUCTS["BoringHelper"].ABI,
+    web3Provider
+  );
+  const feeTo: string = await sushiFactory.feeTo();
+  const pairsLength: number = BigNumber.from(
+    await sushiFactory.allPairsLength()
+  ).toNumber();
+  const pairInfos = await boringHelper.getPairs(
+    sushiFactory.address,
+    0,
+    pairsLength
+  );
+  const pairAddresses = pairInfos.map((pair: any) => {
+    return pair.token;
+  });
+  const tokenAddresses: string[] = [];
+  pairInfos.map((pair: any) => {
+    if (tokenAddresses.indexOf(pair.token0) === -1)
+      tokenAddresses.push(pair.token0);
+    if (tokenAddresses.indexOf(pair.token1) === -1)
+      tokenAddresses.push(pair.token1);
+  });
+  const prices = await getCoingeckoPrices(
+    NETWORKS[chainId].coingeckoId,
+    tokenAddresses
+  );
+  console.log(prices);
+  const pairBalances = await boringHelper.pollPairs(feeTo, pairAddresses);
+  return pairInfos.map((pair: any, index: number): IPairData => {
+    const pairBalance = pairBalances[index];
+    const totalSupply: number = parseFloat(
+      formatUnits(pairBalance.totalSupply)
+    );
+    const balance: number = parseFloat(formatUnits(pairBalance.balance));
+    const share: number = !isNaN(balance / totalSupply)
+      ? balance / totalSupply
+      : 0;
+    const token0Price: number =
+      prices[pair.token0.toLowerCase()] !== undefined
+        ? prices[pair.token0.toLowerCase()]
+        : 0;
+    const token1Price: number =
+      prices[pair.token1.toLowerCase()] !== undefined
+        ? prices[pair.token1.toLowerCase()]
+        : 0;
+    const token0Value: number =
+      parseFloat(
+        formatUnits(
+          pairBalance.reserve0,
+          getToken(pair.token0, chainId).decimals
+        )
+      ) * token0Price;
+    const token1Value: number =
+      parseFloat(
+        formatUnits(
+          pairBalance.reserve1,
+          getToken(pair.token1, chainId).decimals
+        )
+      ) * token1Price;
+    const value: number = (token0Value + token1Value) * share;
+    return {
+      name:
+        getToken(pair.token0, chainId).symbol +
+        "/" +
+        getToken(pair.token1, chainId).symbol,
+      tokenA: pair.token0,
+      tokenB: pair.token1,
+      value: value,
+    };
+  });
+}
+
+export async function getCoingeckoPrices(
+  coingeckoChainId: string,
+  addresses: string[]
+) {
+  const prices: {[address: string]: number} = {};
+  for (let i = 0; i < addresses.length; i += 100) {
+      const tempAddr = addresses.slice(i, i + 100);
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/simple/token_price/${coingeckoChainId}?contract_addresses=${tempAddr.join(
+        ","
+      )}&vs_currencies=usd`,
+      { method: "GET" }
+    );
+    const temp = await res.json();
+    Object.keys(temp).map((address: string) => {
+        prices[address] = temp[address].usd;
+    });
+  }
+  return prices;
+}
