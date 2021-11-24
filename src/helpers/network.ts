d568eab568f48803615cae62851a3acd397aea42
HHK <hhk.contact@protonmail.com>
2021-11-24 17:53:31 +0100

    feat: add coingecko chain id in networks

--- src/helpers/network.ts
+++ src/helpers/network.ts
@@ -13,7 +13,8 @@ export interface INetwork {
     id: number,
     name: string,
     logo: string
-    rpc: string
+    rpc: string,
+    coingeckoId: string
 }
 
 export enum CHAIN_IDS {
@@ -34,60 +35,70 @@ export const NETWORKS: {[id: number]: INetwork} = {
         id: 42161,
         name: "Arbitrum",
         logo: arbitrum,
-        rpc: "https://arb1.arbitrum.io/rpc"
+        rpc: "https://arb1.arbitrum.io/rpc",
+        coingeckoId: "arbitrum-one"
     },
     [CHAIN_IDS.AVALANCHE]: {
         id: 43114,
         name: "Avalanche",
         logo: avalanche,
-        rpc: "https://api.avax.network/ext/bc/C/rpc"
+        rpc: "https://api.avax.network/ext/bc/C/rpc",
+        coingeckoId: "avalanche"
     },
     [CHAIN_IDS.BSC]: {
         id: 56,
         name: "BSC",
         logo: bsc,
-        rpc: "https://bsc-dataseed1.binance.org"
+        rpc: "https://bsc-dataseed1.binance.org",
+        coingeckoId: "binance-smart-chain"
     },
     [CHAIN_IDS.CELO]: {
         id: 42220,
         name: "Celo",
         logo: celo,
-        rpc: "https://forno.celo.org"
+        rpc: "https://forno.celo.org",
+        coingeckoId: "celo"
     },
     [CHAIN_IDS.ETHEREUM]: {
         id: 1,
         name: "Ethereum",
         logo: ethereum,
-        rpc: "https://api.mycryptoapi.com/eth"
+        rpc: "https://api.mycryptoapi.com/eth",
+        coingeckoId: "ethereum"
     },
     [CHAIN_IDS.FANTOM]: {
         id: 250,
         name: "Fantom",
         logo: fantom,
-        rpc: "https://rpc.ftm.tools"
+        rpc: "https://rpc.ftm.tools",
+        coingeckoId: "fantom"
     },
     [CHAIN_IDS.HARMONY]: {
         id: 1666600000,
         name: "Harmony",
         logo: harmony,
-        rpc: "https://api.harmony.one"
+        rpc: "https://api.harmony.one",
+        coingeckoId: "harmony-shard-0"
     },
     [CHAIN_IDS.MOONRIVER]: {
         id: 1285,
         name: "Moonriver",
         logo: moonriver,
-        rpc: "https://rpc.moonriver.moonbeam.network"
+        rpc: "https://rpc.moonriver.moonbeam.network",
+        coingeckoId: "moonriver"
     },
     [CHAIN_IDS.POLYGON]: {
         id: 137,
         name: "Polygon",
         logo: polygon,
-        rpc: "https://polygon-rpc.com/"
+        rpc: "https://polygon-rpc.com/",
+        coingeckoId: "polygon-pos"
     },
     [CHAIN_IDS.XDAI]: {
         id: 100,
         name: "XDAI",
         logo: xdai,
-        rpc: "https://rpc.xdaichain.com"
+        rpc: "https://rpc.xdaichain.com",
+        coingeckoId: "xdai"
     },
 }
