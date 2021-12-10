2b97136156275139944cc4c239b5981c3a903abd
HHK <hhk.contact@protonmail.com>
2021-12-10 13:29:06 +0100

    style: reformat + remove unused faq.json

--- src/imports/kashiPairs/index.ts
+++ src/imports/kashiPairs/index.ts
@@ -1,26 +1,26 @@
-import kashiPairsEthereum from "./ethereum.json";
-import kashiPairsArbitrum from "./arbitrum.json";
-import kashiPairsBsc from "./bsc.json";
-import kashiPairsPolygon from "./polygon.json";
-import kashiPairsXdai from "./xdai.json";
-import {IToken} from "./../tokens";
-import { CHAIN_IDS } from "../../helpers/network";
+import kashiPairsEthereum from './ethereum.json';
+import kashiPairsArbitrum from './arbitrum.json';
+import kashiPairsBsc from './bsc.json';
+import kashiPairsPolygon from './polygon.json';
+import kashiPairsXdai from './xdai.json';
+import { IToken } from './../tokens';
+import { CHAIN_IDS } from '../../helpers/network';
 
-export const KASHI_PAIRS: {[id: number]: string[]} = {
-    [CHAIN_IDS.ETHEREUM]: kashiPairsEthereum,
-    [CHAIN_IDS.ARBITRUM]: kashiPairsArbitrum,
-    [CHAIN_IDS.BSC]: kashiPairsBsc,
-    [CHAIN_IDS.POLYGON]: kashiPairsPolygon,
-    [CHAIN_IDS.XDAI]: kashiPairsXdai
-}
+export const KASHI_PAIRS: { [id: number]: string[] } = {
+  [CHAIN_IDS.ETHEREUM]: kashiPairsEthereum,
+  [CHAIN_IDS.ARBITRUM]: kashiPairsArbitrum,
+  [CHAIN_IDS.BSC]: kashiPairsBsc,
+  [CHAIN_IDS.POLYGON]: kashiPairsPolygon,
+  [CHAIN_IDS.XDAI]: kashiPairsXdai,
+};
 
 export interface IKashiPairData {
-    address: string,
-    asset: IToken,
-    collateral: IToken,
-    apr: number,
-    totalAsset: number,
-    totalBorrow: number,
-    utilization: number,
-    userAsset: number
+  address: string;
+  asset: IToken;
+  collateral: IToken;
+  apr: number;
+  totalAsset: number;
+  totalBorrow: number;
+  utilization: number;
+  userAsset: number;
 }
