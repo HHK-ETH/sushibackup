545bbc9ed414c080f71e6983f2a11f2a12fb726c
HHK <hhk.contact@protonmail.com>
2021-12-14 23:47:53 +0100

    feat: add unwindModal to unwindoor

--- src/components/unwindooor/Unwindooor.tsx
+++ src/components/unwindooor/Unwindooor.tsx
@@ -1,11 +1,10 @@
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
-import { providers } from 'ethers';
 import { useEffect, useState } from 'react';
 import { PRODUCTS, PRODUCT_IDS } from '../../helpers/products';
-import { getAllpairs, IPairData } from '../../helpers/sushiMaker';
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, useQuery, gql } from '@apollo/client';
 import { NETWORKS } from '../../helpers/network';
+import UnwindModal from './UnwindModal';
 
 const defaultClient = new ApolloClient({
   uri: NETWORKS[1].exchangeSubgraph,
@@ -22,11 +21,19 @@ const getQuery = (feeTo: string) => {
             reserveUSD
             totalSupply
             name
+            reserve0
+            reserve1
             token0 {
+              id
+              symbol
               name
+              decimals
             }
             token1 {
+              id
               name
+              symbol
+              decimals
             }
           }
           liquidityTokenBalance
@@ -37,11 +44,19 @@ const getQuery = (feeTo: string) => {
             reserveUSD
             totalSupply
             name
+            reserve0
+            reserve1
             token0 {
+              id
               name
+              symbol
+              decimals
             }
             token1 {
+              id
               name
+              symbol
+              decimals
             }
           }
           liquidityTokenBalance
@@ -68,7 +83,7 @@ const Unwindooor = (): JSX.Element => {
   const [openModal, setOpenModal]: [boolean, Function] = useState(false);
 
   useEffect(() => {
-    if (active && chainId && NETWORKS[chainId]) {
+    if (active && chainId && chainId !== 1 && NETWORKS[chainId]) {
       setClient(
         new ApolloClient({
           uri: NETWORKS[chainId].exchangeSubgraph,
@@ -81,6 +96,7 @@ const Unwindooor = (): JSX.Element => {
 
   useEffect(() => {
     if (data) {
+      if (!data.users[0]) return;
       let fees = 0;
       [...data.users[0].lp1, ...data.users[0].lp2].forEach((position: any) => {
         const pair = position.pair;
@@ -96,70 +112,74 @@ const Unwindooor = (): JSX.Element => {
   }
   if (loading) return <div className="container p-16 mx-auto text-center text-white">Loading data...</div>;
   if (error) return <div className="container p-16 mx-auto text-center text-white">Subgraph returned an error.</div>;
+  if (!data.users[0]) return <div className="container p-16 mx-auto text-center text-white">Nothing to unwind.</div>;
 
   return (
-    <div className="container p-16 mx-auto text-center text-white">
-      <h1 className="text-xl">Total fees available: {totalFees.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}$</h1>
-      {loading && <div className={'text-white'}>loading data...</div>}
-      {selectedPairs.length > 0 && (
-        <button
-          className="float-right px-12 text-lg font-medium text-white bg-pink-500 rounded hover:bg-pink-600 focus:outline-none"
-          onClick={() => setOpenModal(true)}
-        >
-          {}
-        </button>
-      )}
-      <div className="grid grid-cols-7 py-8 bg-indigo-900 rounded-t-xl">
-        <div className="">Pair</div>
-        <div className="col-span-2">Token A</div>
-        <div className="col-span-2">Token B</div>
-        <div className="">Value</div>
-        <div className="">Select</div>
-      </div>
-      {[...data.users[0].lp1, ...data.users[0].lp2]
-        .sort((positionA: any, positionB: any) => {
-          const pairA = positionA.pair;
-          const valueA = (positionA.liquidityTokenBalance / pairA.totalSupply) * pairA.reserveUSD;
-          const pairB = positionB.pair;
-          const valueB = (positionB.liquidityTokenBalance / pairB.totalSupply) * pairB.reserveUSD;
-          if (valueA > valueB) return -1;
-          return +1;
-        })
-        .map((position: any, i: number) => {
-          const pair = position.pair;
-          const value = (position.liquidityTokenBalance / pair.totalSupply) * pair.reserveUSD;
-          return (
-            <div
-              key={i}
-              className="grid grid-cols-7 py-4 bg-indigo-900 cursor-pointer bg-opacity-60 hover:bg-opacity-75"
-            >
-              <div className="">{pair.name}</div>
-              <div className="col-span-2">{pair.token0.name}</div>
-              <div className="col-span-2">{pair.token1.name}</div>
-              <div className="">{value.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}$</div>
-              <div>
-                <input
-                  type={'checkbox'}
-                  onChange={(e) => {
-                    const tempPairs = [...selectedPairs];
-                    if (e.target.checked) {
-                      tempPairs.push(pair.id);
-                      setSelectedPairs(tempPairs);
-                    } else {
-                      setSelectedPairs(
-                        tempPairs.filter((id: string) => {
-                          return id === pair.id ? false : true;
-                        })
-                      );
-                    }
-                  }}
-                  checked={selectedPairs.indexOf(pair.id) !== -1}
-                />
+    <>
+      <UnwindModal openModal={openModal} setOpenModal={setOpenModal} pairs={selectedPairs} />
+      <div className="container p-16 mx-auto text-center text-white">
+        <h1 className="text-xl">Total fees available: {totalFees.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}$</h1>
+        {loading && <div className={'text-white'}>loading data...</div>}
+        {selectedPairs.length > 0 && (
+          <button
+            className="absolute px-16 text-lg font-medium text-white bg-pink-500 rounded bottom-6 right-6 hover:bg-pink-600"
+            onClick={() => setOpenModal(true)}
+          >
+            Unwind!
+          </button>
+        )}
+        <div className="grid grid-cols-7 py-8 bg-indigo-900 rounded-t-xl">
+          <div className="">Pair</div>
+          <div className="col-span-2">Token A</div>
+          <div className="col-span-2">Token B</div>
+          <div className="">Value</div>
+          <div className="">Select</div>
+        </div>
+        {[...data.users[0].lp1, ...data.users[0].lp2]
+          .sort((positionA: any, positionB: any) => {
+            const pairA = positionA.pair;
+            const valueA = (positionA.liquidityTokenBalance / pairA.totalSupply) * pairA.reserveUSD;
+            const pairB = positionB.pair;
+            const valueB = (positionB.liquidityTokenBalance / pairB.totalSupply) * pairB.reserveUSD;
+            if (valueA > valueB) return -1;
+            return +1;
+          })
+          .map((position: any, i: number) => {
+            const pair = position.pair;
+            const value = (position.liquidityTokenBalance / pair.totalSupply) * pair.reserveUSD;
+            return (
+              <div
+                key={i}
+                className="grid grid-cols-7 py-4 bg-indigo-900 cursor-pointer bg-opacity-60 hover:bg-opacity-75"
+              >
+                <div className="">{pair.name}</div>
+                <div className="col-span-2">{pair.token0.name}</div>
+                <div className="col-span-2">{pair.token1.name}</div>
+                <div className="">{value.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}$</div>
+                <div>
+                  <input
+                    type={'checkbox'}
+                    onChange={(e) => {
+                      const tempPairs = [...selectedPairs];
+                      if (e.target.checked) {
+                        tempPairs.push(pair);
+                        setSelectedPairs(tempPairs);
+                      } else {
+                        setSelectedPairs(
+                          tempPairs.filter((p: any) => {
+                            return p.id === pair.id ? false : true;
+                          })
+                        );
+                      }
+                    }}
+                    checked={selectedPairs.indexOf(pair) !== -1}
+                  />
+                </div>
               </div>
-            </div>
-          );
-        })}
-    </div>
+            );
+          })}
+      </div>
+    </>
   );
 };
 
