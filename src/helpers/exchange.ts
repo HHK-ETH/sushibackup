0db6a0464ccfeb3c7972addf71676016ab3dc1ae
HHK <hhk.contact@protonmail.com>
2022-03-24 19:07:12 +0100

    feat: update celo graph url

--- src/helpers/exchange.ts
+++ src/helpers/exchange.ts
@@ -13,7 +13,7 @@ const EXCHANGE_ENDPOINTS: { [chainId: number]: string } = {
   [CHAIN_IDS.ARBITRUM]: 'https://api.thegraph.com/subgraphs/name/sushiswap/arbitrum-exchange',
   [CHAIN_IDS.AVALANCHE]: 'https://api.thegraph.com/subgraphs/name/sushiswap/avalanche-exchange',
   [CHAIN_IDS.BSC]: 'https://api.thegraph.com/subgraphs/name/sushiswap/bsc-exchange',
-  [CHAIN_IDS.CELO]: 'https://api.thegraph.com/subgraphs/name/sushiswap/celo-exchange',
+  [CHAIN_IDS.CELO]: 'https://api.thegraph.com/subgraphs/name/jiro-ono/sushitestsubgraph',
   [CHAIN_IDS.ETHEREUM]: 'https://api.thegraph.com/subgraphs/name/sushiswap/exchange',
   [CHAIN_IDS.FANTOM]: 'https://api.thegraph.com/subgraphs/name/sushiswap/fantom-exchange',
   [CHAIN_IDS.HARMONY]: 'https://sushi.graph.t.hmny.io/subgraphs/name/sushiswap/harmony-exchange',
