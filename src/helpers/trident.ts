fd250d8660ba4a629ede274ededaa61f24f99a32
HHK <hhk.contact@protonmail.com>
2022-03-26 00:23:22 +0100

    feat: add subgraph query

--- src/helpers/trident.ts
+++ src/helpers/trident.ts
@@ -1,7 +1,38 @@
+import request, { gql } from 'graphql-request';
 import { CHAIN_IDS } from './network';
 
 const ROUTER_ADDRESSES: { [chainId: number]: string } = {
   [CHAIN_IDS.POLYGON]: '0xc5017BE80b4446988e8686168396289a9A62668E',
 };
 
-export { ROUTER_ADDRESSES };
+const SUBGRAPH_ENDPOINTS: { [chainId: number]: string } = {
+  [CHAIN_IDS.POLYGON]: 'https://api.thegraph.com/subgraphs/name/sushiswap/trident-polygon',
+};
+
+const TRIDENT_POSITIONS_QUERY = gql`
+  query positions($user: ID!) {
+    liquidityPositions(where: { user: $user }) {
+      pool {
+        id
+        swapFee
+        assets {
+          token {
+            id
+            name
+            symbol
+          }
+        }
+      }
+      balance
+    }
+  }
+`;
+
+const queryTridentPositions = async (chainId: number, user: string): Promise<any> => {
+  const positions = await request(SUBGRAPH_ENDPOINTS[chainId], TRIDENT_POSITIONS_QUERY, {
+    user: user.toLowerCase(),
+  });
+  return positions.liquidityPositions;
+};
+
+export { SUBGRAPH_ENDPOINTS, queryTridentPositions };
