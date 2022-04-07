2574c7c031df2df6c1dee02bb1ca9a0f827da5f6
HHK <hhk.contact@protonmail.com>
2022-04-07 23:39:55 +0200

    feat: add dca helper

--- src/helpers/dca.ts
+++ src/helpers/dca.ts
@@ -1,31 +1,53 @@
 import { CHAIN_IDS } from './network';
 
 const DCA_FACTORY: { [chainId: number]: string } = {
-  [CHAIN_IDS.POLYGON]: '0xE25cb7B7e8cA345807ec3E92A72E86CB85920922',
+  [CHAIN_IDS.POLYGON]: '0x22cE2cac44E09e75751BdB2f3818bC043807b786',
 };
 
 //Any token could be used but we display only most used ones to make it simpler.
-const DCA_TOKENS: { [chainId: number]: { symbol: string; address: string; priceFeed: string }[] } = {
+const DCA_TOKENS: { [chainId: number]: { symbol: string; address: string; priceFeed: string; decimals: number }[] } = {
   [CHAIN_IDS.POLYGON]: [
     {
       symbol: 'DAI',
       address: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
       priceFeed: '0x4746DeC9e833A82EC7C2C1356372CcF2cfcD2F3D',
+      decimals: 18,
+    },
+    {
+      symbol: 'USDC',
+      address: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+      priceFeed: '0xfE4A8cc5b5B2366C1B58Bea3858e81843581b2F7',
+      decimals: 6,
+    },
+    {
+      symbol: 'USDT',
+      address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+      priceFeed: '0x0A6513e40db6EB1b165753AD52E80663aeA50545',
+      decimals: 6,
     },
     {
       symbol: 'WETH',
       address: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
       priceFeed: '0xF9680D99D6C9589e2a93a78A04A279e509205945',
+      decimals: 18,
+    },
+    {
+      symbol: 'WBTC',
+      address: '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6',
+      priceFeed: '0xDE31F8bFBD8c84b5360CFACCa3539B938dd78ae6',
+      decimals: 8,
+    },
+    {
+      symbol: 'WMATIC',
+      address: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
+      priceFeed: '0xF9680D99D6C9589e2a93a78A04A279e509205945',
+      decimals: 18,
     },
     {
       symbol: 'SUSHI',
       address: '0x0b3F868E0BE5597D5DB7fEB59E1CADBb0fdDa50a',
       priceFeed: '0x49B0c695039243BBfEb8EcD054EB70061fd54aa0',
-    },
-    {
-      symbol: 'AAVE',
-      address: '0xD6DF932A45C0f255f85145f286eA0b292B21C90B',
-      priceFeed: '0x72484B12719E23115761D5DA1646945632979bB6',
+      decimals: 18,
     },
   ],
 };
