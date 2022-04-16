edff92113d61967feb352580e1cc8e1e075775b9
HHK <hhk.contact@protonmail.com>
2022-04-16 23:35:47 +0200

    feat: add export for abis (need to update all imports)

---
+++ src/imports/abis/index.ts
@@ -0,0 +1,13 @@
+import sushimaker from './sushiMaker.json';
+import wethmaker from './wethMaker.json';
+import erc20 from './erc20.json';
+import bento from './bento.json';
+import dca from './dca.json';
+import dcaFactory from './dcaFactory.json';
+
+export const SUSHIMAKER_ABI = sushimaker;
+export const WETHMAKER_ABI = wethmaker;
+export const ERC20_ABI = erc20;
+export const BENTO_ABI = bento;
+export const DCA_ABI = dca;
+export const DCA_FACTORY = dcaFactory;
