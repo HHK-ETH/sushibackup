c16031c2c4fc4d044237181ef5cae077f53951eb
HHK <hhk.contact@protonmail.com>
2022-04-19 02:15:18 +0200

    feat: add multicall

---
+++ src/helpers/multicall.ts
@@ -0,0 +1,20 @@
+import { Web3Provider } from '@ethersproject/providers';
+import { BigNumber, Contract } from 'ethers';
+import { MULTICALL_ABI } from '../imports/abis';
+
+const ADDRESS = '0xcA11bde05977b3631167028862bE2a173976CA11';
+
+type Call = {
+  target: string;
+  calldata: string;
+};
+
+const multicall = async (
+  calls: Call[],
+  provider: Web3Provider
+): Promise<{ blockNumber: BigNumber; results: any[] }> => {
+  const multicall = new Contract(ADDRESS, MULTICALL_ABI, provider);
+  return await multicall.aggregate(calls);
+};
+
+export { multicall };
--- src/imports/abis/index.ts
+++ src/imports/abis/index.ts
@@ -4,10 +4,12 @@ import erc20 from './erc20.json';
 import bento from './bento.json';
 import dca from './dca.json';
 import dcaFactory from './dcaFactory.json';
+import multicall from './multicall.json';
 
 export const SUSHIMAKER_ABI = sushimaker;
 export const WETHMAKER_ABI = wethmaker;
 export const ERC20_ABI = erc20;
 export const BENTO_ABI = bento;
 export const DCA_ABI = dca;
-export const DCA_FACTORY = dcaFactory;
+export const DCA_FACTORY_ABI = dcaFactory;
+export const MULTICALL_ABI = multicall;
