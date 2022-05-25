304d7e87b6a92953d0abe579dd0e35d13fa719d7
HHK <hhk.contact@protonmail.com>
2022-05-25 06:48:06 +0200

    fix: fix multicall

--- src/helpers/multicall.ts
+++ src/helpers/multicall.ts
@@ -6,13 +6,13 @@ const ADDRESS = '0xcA11bde05977b3631167028862bE2a173976CA11';
 
 type Call = {
   target: string;
-  calldata: string;
+  callData: string;
 };
 
 const multicall = async (
   calls: Call[],
   provider: Web3Provider
-): Promise<{ blockNumber: BigNumber; results: any[] }> => {
+): Promise<{ blockNumber: BigNumber; returnData: string[] }> => {
   const multicall = new Contract(ADDRESS, MULTICALL_ABI, provider);
   return await multicall.aggregate(calls);
 };
