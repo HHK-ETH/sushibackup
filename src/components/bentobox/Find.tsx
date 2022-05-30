3e0fedc68df5aa2e5152ac842a40f4493438f23d
HHK <hhk.contact@protonmail.com>
2022-05-31 00:44:57 +0200

    feat: add find position bento component

---
+++ src/components/bentobox/Find.tsx
@@ -0,0 +1,38 @@
+import { formatUnits } from 'ethers/lib/utils';
+import { useState } from 'react';
+import useFindPosition from '../../hooks/bento/useFindPosition';
+import useWithdrawBento from '../../hooks/bento/useWithdrawBento';
+
+const Find = ({ account }: { account: string | null | undefined }): JSX.Element => {
+  const [address, setAddress] = useState('');
+  const { position, loading } = useFindPosition(account, address);
+  const withdraw = useWithdrawBento(account, async () => {});
+
+  console.log(position);
+
+  return (
+    <div className="text-center text-white">
+      <h2 className="m-2 text-lg">Token address:</h2>
+      <input
+        className="w-3/4 py-2 mb-4 text-sm text-center bg-indigo-700 rounded-lg"
+        placeholder="Enter token address here"
+        onChange={(e) => setAddress(e.target.value)}
+      />
+      {address !== '' && !loading && (
+        <div className="mt-4 text-lg">
+          <h2>Token symbol: {position.symbol}</h2>
+          <h2>BentoBox balance: {parseFloat(formatUnits(position.balance, position.decimals)).toFixed(4)}</h2>
+          <button
+            className={'px-8 mt-2 font-medium text-white bg-pink-500 rounded hover:bg-pink-600 inline-block'}
+            onClick={() => withdraw(address, position.share)}
+          >
+            Withdraw
+          </button>
+        </div>
+      )}
+      {loading && <div>loading...</div>}
+    </div>
+  );
+};
+
+export default Find;
--- src/components/bentobox/bentobox.tsx
+++ src/components/bentobox/bentobox.tsx
@@ -1,14 +1,18 @@
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
 import { formatUnits, parseUnits } from 'ethers/lib/utils';
+import { useState } from 'react';
 import useFetchbalances from '../../hooks/bento/useFetchBalances';
 import useWithdrawBento from '../../hooks/bento/useWithdrawBento';
+import Modal from '../general/Modal';
+import Find from './Find';
 
 const Bentobox = (): JSX.Element => {
   const context = useWeb3React<Web3Provider>();
   const { active, account } = context;
   const { positions, loading, fetchBalances } = useFetchbalances(account);
   const withdraw = useWithdrawBento(account, fetchBalances);
+  const [open, setOpen] = useState(false);
 
   if (loading) {
     return <div className="text-center text-white">Loading data...</div>;
@@ -19,6 +23,9 @@ const Bentobox = (): JSX.Element => {
 
   return (
     <>
+      <Modal open={open} setOpen={setOpen}>
+        <Find account={account} />
+      </Modal>
       <div className="container p-16 mx-auto text-center text-white">
         <h1 className="text-xl">You have {positions.length} positions in BentoBox.</h1>
         <div className="grid grid-cols-3 mt-2 text-xl bg-indigo-900 rounded-t-xl">
@@ -26,28 +33,32 @@ const Bentobox = (): JSX.Element => {
           <div>Balance</div>
           <div>Action</div>
         </div>
-        <div className="rounded-b-xl">
-          {positions.map((position: any, index: number) => {
-            return (
-              <div
-                key={index}
-                className="grid grid-cols-3 py-2 bg-indigo-900 text-md bg-opacity-60 hover:bg-opacity-75"
-              >
-                <div>
-                  {position.token.name} ({position.token.symbol})
-                </div>
-                <div>{formatUnits(position.amount, position.token.decimals)}</div>
-                <div>
-                  <button
-                    className={'px-8 font-medium text-white bg-pink-500 rounded hover:bg-pink-600 inline-block'}
-                    onClick={() => withdraw(position.token.id, parseUnits(position.share, position.token.decimals))}
-                  >
-                    Withdraw
-                  </button>
-                </div>
+        {positions.map((position: any, index: number) => {
+          return (
+            <div key={index} className="grid grid-cols-3 py-2 bg-indigo-900 text-md bg-opacity-60 hover:bg-opacity-75">
+              <div>
+                {position.token.name} ({position.token.symbol})
               </div>
-            );
-          })}
+              <div>{formatUnits(position.amount, position.token.decimals)}</div>
+              <div>
+                <button
+                  className={'px-8 font-medium text-white bg-pink-500 rounded hover:bg-pink-600 inline-block'}
+                  onClick={() => withdraw(position.token.id, parseUnits(position.share, position.token.decimals))}
+                >
+                  Withdraw
+                </button>
+              </div>
+            </div>
+          );
+        })}
+        <div className="inline-block p-4 mt-6 bg-indigo-900 rounded-lg">
+          <h1 className="mb-2 text-xl">Position missing ?</h1>
+          <button
+            className={'px-8 font-medium text-white bg-pink-500 rounded hover:bg-pink-600 inline-block'}
+            onClick={() => setOpen(true)}
+          >
+            Find a position manually
+          </button>
         </div>
       </div>
     </>
