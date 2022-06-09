57846ef4bbd69ac335b803e3e4064e92c0df0c53
HHK <hhk.contact@protonmail.com>
2022-06-09 12:00:18 +0200

    fix: remove sm tailwind override

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      screens: {
-        sm: { sm: { min: '0px', max: '767px' } },
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
